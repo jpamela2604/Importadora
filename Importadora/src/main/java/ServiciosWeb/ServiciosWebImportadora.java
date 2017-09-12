187072b9489a6e09c924f761359bc6127799d840
avillatoro <lukycrazy@gmail.com>
2017-09-12 13:32:39 -0600

    Ana: Servicio y su respectivo test en Impoertadora

--- Importadora/src/main/java/ServiciosWeb/ServiciosWebImportadora.java
+++ Importadora/src/main/java/ServiciosWeb/ServiciosWebImportadora.java
@@ -25,44 +25,74 @@ public class ServiciosWebImportadora {
     }*/
     
     @WebMethod(operationName = "validar_Sesion")
-    public Boolean validar_Sesion(@WebParam(name = "username") String username,@WebParam(name = "password") String password) {
+    public String validar_Sesion(@WebParam(name = "username") String username,@WebParam(name = "password") String password) {
         Boolean respuesta = false;
         if (username=="sa"&&password=="1234")
         {
             respuesta=true;
         }
-        return respuesta;
+        String salidajson = "{\"respuesta\" : \"" + respuesta + "\"}";
+        return salidajson;
     }
     
     @WebMethod(operationName = "crear_Cuenta")
-    public Boolean crear_Cuenta(@WebParam(name = "nombre") String nombre,
+    public String crear_Cuenta(@WebParam(name = "nombre") String nombre,
             @WebParam(name = "username") String username,
             @WebParam(name = "password") String password,
             @WebParam(name = "no_Tarjeta") String no_Tarjeta) 
-    {
+    {   //hacer validacion de numero de tarjeta
+        // verificar que no existe ese username
+        
         Boolean respuesta = false;
-        return respuesta;
+        String salidajson = "{\"respuesta\" : \"" + respuesta + "\"}";
+        return salidajson;
     }
     
     @WebMethod(operationName = "solicitar_Catalogo_Vehiculos")
     public String solicitar_Catalogo_Vehiculos() {
-        String listado_Vehiculos="";
+        String id_Vehiculo="-1";
+        String marca="-1";
+        String linea="-1";
+        String modelo="-1";
         
-        return listado_Vehiculos;
+        String salidajson = "{\"vehiculos\" : [{\"id_Vehiculo\" : " + id_Vehiculo + ",\"marcar\" : \"" + marca + "\",\"linea\" : \"" + linea + "\", \"modelo\" : \"" + modelo + "\"}]";
+        return salidajson;
     }
     
     @WebMethod(operationName = "cotizar_Vehiculo")
-    public String cotizar_Vehiculo(@WebParam(name = "linea") String linea,@WebParam(name = "modelo") String modelo) {
-        String listado_Cotizaciones ="";        
-        return listado_Cotizaciones;
+    public String cotizar_Vehiculo(@WebParam(name = "id_Vehiculo") Integer id_Vehiculo,
+            @WebParam(name = "marca") String marca,
+            @WebParam(name = "linea") String linea,
+            @WebParam(name = "modelo") String modelo) {
+        double precio_Vehiculo= -1.0;
+        double precio_Envio=-1.0;
+        double impuesto_Sat=-1.0;
+        double impuesto_Aduana=-1.0;
+        double iva=-1.0;
+        double isr=-1.0;
+        
+        String salidajson = "{ \"precio_Vehiculo\" : "+precio_Vehiculo+", \"precio_Envio\": "+precio_Envio+", \"impuesto_Sat\": "+impuesto_Sat+", \"impuesto_Aduana\" : "+impuesto_Aduana+", \"iva\" : "+iva+", \"isr\": "+isr+"}";
+        return salidajson;
+        
     }
     
      @WebMethod(operationName = "comprar_Vehículo")
     public String comprar_Vehículo(@WebParam(name = "id_Cliente") Integer id_Cliente,
             @WebParam(name = "no_Tarjeta") String no_Tarjeta,
-            @WebParam(name = "id_Vehiculo") Integer id_Vehiculo
+            @WebParam(name = "id_Vehiculo") Integer id_Vehiculo,
+            @WebParam(name = "precio_Vehiculo") double precio_Vehiculo,
+            @WebParam(name = "precio_Envio") double precio_Envio,
+            @WebParam(name = "impuesto_Sat") double impuesto_Sat,            
+            @WebParam(name = "impuesto_Aduana") double impuesto_Aduana,
+            @WebParam(name = "iva") double iva,
+            @WebParam(name = "isr") double isr,
+            @WebParam(name = "pais_Origen ") String pais_Origen,
+            @WebParam(name = "pais_Destino  ") String pais_Destino 
     ) {
-        String respuesta="";
-        return respuesta;
+        String A="A";
+        Integer noFac=-1;
+        String salidajson="{\"serie\" : \""+A+"\",\"numero_Factura\" : \""+noFac+"\" }";
+        
+        return salidajson;
     }
 }
--- Importadora/src/test/java/ServiciosWebTest/ServiciosWebImportadoraTest.java
+++ Importadora/src/test/java/ServiciosWebTest/ServiciosWebImportadoraTest.java
@@ -47,30 +47,30 @@ public class ServiciosWebImportadoraTest {
     // public void hello() {}
     @Test  
      public void prueba_servicio_validar_Sesion(){
-         boolean r =  this.request.validar_Sesion("sa", "1234");
-         assertEquals(true,r);
+         String r =  this.request.validar_Sesion("sa", "1234");
+         assertEquals("{\"respuesta\" : \"true\"}",r);
      }
      
      @Test  
      public void prueba_servicio_crear_Cuenta(){
-         boolean r =  this.request.crear_Cuenta("Pamela Palacios", "pam_pam", "pam123", "125-9632-524");
-         assertEquals(false,r);
+         String r =  this.request.crear_Cuenta("Pamela Palacios", "pam_pam", "pam123", "125-9632-524");
+         assertEquals("{\"respuesta\" : \"false\"}",r);
      }
      
      @Test  
      public void prueba_servicio_solicitar_Catalogo_Vehiculos(){
          String r =  this.request.solicitar_Catalogo_Vehiculos();
-         assertEquals("",r);
+         assertEquals("{\"vehiculos\" : [{\"id_Vehiculo\" : -1,\"marcar\" : \"-1\",\"linea\" : \"-1\", \"modelo\" : \"-1\"}]",r);
      }
      @Test  
      public void prueba_servicio_cotizar_Vehiculo(){
          //linea/modelo
-         String r =  this.request.cotizar_Vehiculo("C320", "2004");
-         assertEquals("",r);
+         String r =  this.request.cotizar_Vehiculo(15,"lamarca","C320", "2004");
+         assertEquals("{ \"precio_Vehiculo\" : -1.0, \"precio_Envio\": -1.0, \"impuesto_Sat\": -1.0, \"impuesto_Aduana\" : -1.0, \"iva\" : -1.0, \"isr\": -1.0}",r);
      }
      @Test  
      public void prueba_servicio_comprar_Vehículo(){
-         String r =  this.request.comprar_Vehículo(25, "125-9632-524", 75);
-         assertEquals("",r);
+         String r =  this.request.comprar_Vehículo(25, "125-9632-524", 75,100.20,200.00,50.01,40.00,60.00,100.00,"Honduas","Panama");
+         assertEquals("{\"serie\" : \"A\",\"numero_Factura\" : \"-1\" }",r);
      }
 }
